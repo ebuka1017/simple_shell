c7c463f301f88bd598e80919f6eca74b655b94d8
Omarr-kh <omar1752003@gmail.com>
2023-07-22 13:33:54 -0700

    fixed some issues

--- file_io_func.c
+++ file_io_func.c
@@ -1,51 +1,89 @@
 #include "shell.h"
 
 /**
+ * build_hist - builds history list
+ * @f_info: struct address
+ * @buffer: buffer variable
+ * @line_count: lines count
+ * Return: int
+ */
+int build_hist(info_t *f_info, char *buffer, int line_count)
+{
+	list_t *node = NULL;
+
+	if (f_info->hist)
+		node = f_info->hist;
+	add_node(&node, buffer, line_count);
+
+	if (!f_info->hist)
+		f_info->hist = node;
+	return (0);
+}
+
+/**
+ * renum_hist - renumbers the history
+ * @f_info: struct address
+ * Return: hist count
+ */
+int renum_hist(info_t *f_info)
+{
+	list_t *node = f_info->hist;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (f_info->hist_count = i);
+}
+
+/**
  * read_hist - reads history file
  * @f_info: struct address
  * Return: hist_count, 0 otherwise
  */
 int read_hist(info_t *f_info)
 {
-	int i, last = 0, linecount = 0;
-	ssize_t fd, rdlen, fsize = 0;
-	struct stat st;
-	char *buffer = NULL, *fname = get_hist(f_info);
+	int i, l = 0, lines_count = 0;
+	ssize_t fd, read_len, file_size = 0;
+	struct stat s;
+	char *buffer = NULL, *filename = get_hist(f_info);
 
-	if (!fname)
+	if (!filename)
 		return (0);
 
-	fd = open(fname, O_RDONLY);
-	free(fname);
+	fd = open(filename, O_RDONLY);
+	free(filename);
 	if (fd == -1)
 		return (0);
-	if (!fstat(fd, &st))
-		fsize = st.st_size;
-	if (fsize < 2)
+	if (!fstat(fd, &s))
+		file_size = s.st_size;
+	if (file_size < 2)
 		return (0);
-	buffer = malloc(sizeof(char) * (fsize + 1));
+	buffer = malloc(sizeof(char) * (file_size + 1));
 
 	if (!buffer)
 		return (0);
-	rdlen = read(fd, buffer, fsize);
-	buffer[fsize] = 0;
-	if (rdlen <= 0)
+	read_len = read(fd, buffer, file_size);
+	buffer[file_size] = 0;
+	if (read_len <= 0)
 		return (free(buffer), 0);
 	close(fd);
-	for (i = 0; i < fsize; i++)
+	for (i = 0; i < file_size; i++)
 		if (buffer[i] == '\n')
 		{
 			buffer[i] = 0;
-			build_hist(f_info, buffer + last, linecount++);
-			last = i + 1;
+			build_hist(f_info, buffer + l, lines_count++);
+			l = i + 1;
 		}
-	if (last != i)
-		build_hist(f_info, buffer + last, linecount++);
+	if (l != i)
+		build_hist(f_info, buffer + l, lines_count++);
 	free(buffer);
-	f_info->hist_count = linecount;
+	f_info->hist_count = lines_count;
 	while (f_info->hist_count-- >= HIST_MAX)
 		delete_node(&(f_info->hist), 0);
-	renumber_hist(f_info);
+	renum_hist(f_info);
 	return (f_info->hist_count);
 }
 
